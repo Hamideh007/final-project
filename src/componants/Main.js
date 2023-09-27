8773c905df11c5365e0b5959ab0d9c5cfdd3afa7
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-09-27 19:39:08 -0400

    add Main.js

---
+++ src/componants/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="hero">
+        <h2 className="subheader">Movie app</h2>
+        <input type="text" placeholder="search your favorate movies hear" />
+      </div>
+    </main>
+  );
+}
+export default Main;
