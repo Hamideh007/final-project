94d0c90b30c99fe981520d8db8abaf3f86216c01
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-09-27 19:30:36 -0400

    add nav and Header.js

---
+++ src/componants/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Movies</li>
+        <li>Sign In</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
