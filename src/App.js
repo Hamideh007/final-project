43c17c1ebfea943b831a8364e2504fd13c7469c4
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-09-27 21:46:27 -0400

    add container.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer/>
+      <Container />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Container() {
+  return (
+    <section className="main-section">
+      <h3 className="subtitle">Explore more </h3>
+      <div className="grid">     
+      </div>
+    </section>
+  );
+}
+
+export default Container;
